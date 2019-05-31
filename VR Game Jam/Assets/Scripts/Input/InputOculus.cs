c78ec63dcef9415b66c86f947967ed003329a1e4
Dillon Handyside <dj_handyside@hotmail.com>
2019-05-31 13:42:14 +1000

    Started implementation on Oculus input, e.g. Raycasts, LineRenderer etc.

---
+++ VR Game Jam/Assets/Scripts/Input/InputOculus.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Shooter))]
+public class InputOculus : MonoBehaviour
+{
+    [SerializeField] private Transform m_rightHandAnchor;
+
+    private Shooter m_shooter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_shooter = GetComponent<Shooter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 origin = m_rightHandAnchor.position;
+        Vector3 direction = m_rightHandAnchor.forward;
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            m_shooter.Fire(origin, direction);
+        }
+    }
+}
