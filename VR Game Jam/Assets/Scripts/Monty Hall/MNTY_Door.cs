42f9ad9457f90ce4832103239e19d169b1a154a8
Sam <sammydeedge@hotmail.com>
2019-06-06 16:50:48 +1000

    Added materials and prefabs
    And hopefully didn't break things

---
+++ VR Game Jam/Assets/Scripts/Monty Hall/MNTY_Door.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MNTY_Door : MonoBehaviour
+{
+    public bool willBeOpened;
+    public bool isOpening;
+
+    Vector3 start_rot =  new Vector3(0.0f,0.0f,0.0f);
+    Vector3 end_rot = new Vector3(0.0f, 120.0f, 0.0f);
+
+    float timer;
+    public float doorOpenTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        willBeOpened = false;
+        isOpening = false;
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpening)
+        {
+            timer += Time.deltaTime;
+            float percentDone = timer / doorOpenTime;
+            Quaternion current_rot = Quaternion.Euler(Vector3.Lerp(start_rot, end_rot, percentDone));
+            transform.rotation = current_rot;
+            if (percentDone > 0.999f)
+            {
+                isOpening = false;
+                timer = 0.0f;
+            }
+        }
+    }
+
+    public void setWillBeOpened(bool choice)
+    {
+        willBeOpened = choice;
+    }
+
+    public void onDrawSignal()
+    {
+        if (willBeOpened) { isOpening = true; /*runDoorOpen();*/ }
+    }
+
+    void runDoorOpen()
+    {
+        isOpening = true;
+    }
+}
---
+++ VR Game Jam/Assets/Scripts/Monty Hall/MNTY_Umpire.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MNTY_Umpire : MonoBehaviour
+{
+    List<GameObject> doorList;
+
+    [Range(0,5)]
+    public int LVL1_opponents;
+    [Range(0, 5)]
+    public int LVL1_bystanders;
+    [Range(0, 5)]
+    public int LVL2_opponents;
+    [Range(0, 5)]
+    public int LVL2_bystanders;
+    [Range(0, 5)]
+    public int LVL3_opponents;
+    [Range(0, 5)]
+    public int LVL3_bystanders;
+
+    Vector3 offset;
+
+    public GameObject prefab_SuccessTarget;
+    public GameObject prefab_FailureTarget;
+
+    [Range(1, 3)]
+    public int testDifficulty;
+
+    bool isDrawSignalSent;
+
+    void Start()
+    {
+        doorList = new List<GameObject>();
+        for (int i = 1; i < 6; i++)
+        {
+            string doorName = "Door" + i.ToString();
+
+            GameObject doorOBJ = GameObject.Find(doorName);
+            doorList.Add(doorOBJ);
+        }
+        offset = new Vector3(1.55f, 0.0f, 1.1f);
+        setupMNTY(testDifficulty);
+        isDrawSignalSent = false;
+    }
+
+    public void setupMNTY(int difficulty) //1 for easy up to 3 for hard
+    {
+        int opponents = 0;
+        int bystanders = 0;
+        if      (difficulty == 1)       { opponents = LVL1_opponents;   bystanders = LVL1_bystanders; }
+        else if (difficulty == 2)       { opponents = LVL2_opponents;   bystanders = LVL2_bystanders; }
+        else if (difficulty == 3)       { opponents = LVL3_opponents;   bystanders = LVL3_bystanders; }
+        else                            { Debug.Log("Error Invalid Difficulty set."); }
+
+        runDoorSetup(opponents,bystanders);
+    }
+
+    void runDoorSetup(int opponents, int bystanders)
+    {
+        List<GameObject> temp_doors = new List<GameObject>(doorList);
+
+        for (int o = 0; o < opponents; o++)
+        {
+            int doorListSelect = UnityEngine.Random.Range(0, temp_doors.Count - 1);
+            GameObject selectedDoor = temp_doors[doorListSelect];
+            selectedDoor.GetComponent<MNTY_Door>().setWillBeOpened(true);
+            GameObject go = Instantiate(prefab_SuccessTarget);
+            //go.transform.SetParent(selectedDoor.transform);
+            go.transform.position = selectedDoor.transform.position + offset;
+
+            temp_doors.Remove(selectedDoor);
+        }
+
+        for (int b = 0; b < bystanders; b++)
+        {
+            int doorListSelect = UnityEngine.Random.Range(0, temp_doors.Count - 1);
+            GameObject selectedDoor = temp_doors[doorListSelect];
+            selectedDoor.GetComponent<MNTY_Door>().setWillBeOpened(true);
+            GameObject go = Instantiate(prefab_FailureTarget);
+            //go.transform.SetParent(selectedDoor.transform);
+            go.transform.position = selectedDoor.transform.position + offset;
+
+            temp_doors.Remove(selectedDoor);
+        }
+
+        //foreach (GameObject leftover in temp_doors)
+        //{
+        //    leftover.GetComponent<MNTY_Door>().setWillBeOpened(false);
+        //}
+
+    }
+
+    void Update()
+    {
+        if (UmpireControl.isGameStarted && !isDrawSignalSent)
+        {
+            onDrawSignal();
+            isDrawSignalSent = true;
+        }
+    }
+
+    public void onDrawSignal()
+    {
+        foreach (GameObject door in doorList)
+        {
+            door.GetComponent<MNTY_Door>().onDrawSignal();
+        }
+    }
+
+}
