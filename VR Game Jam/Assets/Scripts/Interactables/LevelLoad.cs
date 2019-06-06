78fb5bd3adbd3a50d321ef8ff184f1c41d0d6a09
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2019-06-06 16:42:17 +1000

    Bar scene, Main Menu.

---
+++ VR Game Jam/Assets/Scripts/Interactables/LevelLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : Interactable
+{
+    public int levelToLoad;
+
+    public override void OnInteract()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+}
