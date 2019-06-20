a82b670a5b06f124600341e5e3c81e502ead0ba6
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2019-06-20 16:15:45 +1000

    sTUFF

--- VR Game Jam/Assets/Scripts/Bird in the Bush/Bird_Umpire.cs
+++ VR Game Jam/Assets/Scripts/Bird in the Bush/Bird_Umpire.cs
@@ -43,6 +43,8 @@ public class Bird_Umpire : MonoBehaviour
 
         float SPWN_leftSideZ = m_spawnLeftGuide.transform.position.z;
         float SPWN_rightSideZ = m_spawnRightGuide.transform.position.z;
+        float SPWN_leftSideX = m_spawnLeftGuide.transform.position.x;
+        float SPWN_rightSideX = m_spawnRightGuide.transform.position.x;
 
         float SPWNStart_YMin, SPWNStart_YMax, SPWNEnd_YMin, SPWNEnd_YMax;
 
@@ -50,12 +52,14 @@ public class Bird_Umpire : MonoBehaviour
         if (LRRand == 0)
         {
             bird_startPos.z = SPWN_leftSideZ;   bird_endPos.z = SPWN_rightSideZ;
+            bird_startPos.x = SPWN_leftSideX; bird_endPos.x = SPWN_rightSideX;
             SPWNStart_YMin = SPWNLeft_YMin;     SPWNStart_YMax = SPWNLeft_YMax;
             SPWNEnd_YMin = SPWNRight_YMin;      SPWNEnd_YMax = SPWNRight_YMax;
         }
         else
         {
             bird_startPos.z = SPWN_rightSideZ;  bird_endPos.z = SPWN_leftSideZ;
+            bird_startPos.x = SPWN_rightSideX; bird_endPos.x = SPWN_leftSideX;
             SPWNStart_YMin = SPWNRight_YMin;    SPWNStart_YMax = SPWNRight_YMax;
             SPWNEnd_YMin = SPWNLeft_YMin;       SPWNEnd_YMax = SPWNLeft_YMax;
         }
