012202fc3f0f2ece135aa553ffbe7b2adfda8ad9
Dillon Handyside <dj_handyside@hotmail.com>
2019-06-21 15:35:05 +1000

    fuck^2

---
+++ VR Game Jam/Assets/Scripts/Interactables/Spinner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Animator))]
+public class Spinner : Interactable
+{
+    [Header("Spinning")]
+    [SerializeField] private float m_spinDelay = 0.5f;
+    private bool m_canSpin = true;
+
+    [Header("Other Dependancies")]
+    [SerializeField] private Animator m_animator;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public override void OnInteract()
+    {
+        if (m_canSpin)
+            StartCoroutine(Spin());
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Spin()
+    {
+        m_animator.SetTrigger("spin");
+        m_canSpin = false;
+
+        yield return new WaitForSeconds(m_spinDelay);
+
+        m_canSpin = true;
+    }
+}
