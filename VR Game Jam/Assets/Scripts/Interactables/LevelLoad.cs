afdd1d9fe7fcb390bba29f9057f792b9fda202ea
Dillon Handyside <dj_handyside@hotmail.com>
2019-06-21 10:01:14 +1000

    spin to win

--- VR Game Jam/Assets/Scripts/Interactables/LevelLoad.cs
+++ VR Game Jam/Assets/Scripts/Interactables/LevelLoad.cs
@@ -7,8 +7,35 @@ public class LevelLoad : Interactable
 {
     public int levelToLoad;
 
+    [SerializeField] private float m_loadDelay = 0.5f;
+    private Animator m_animator;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
     public override void OnInteract()
     {
+        StartCoroutine(Spin());
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Spin()
+    {
+        m_animator.SetTrigger("spin");
+
+        yield return new WaitForSeconds(m_loadDelay);
+
         FindObjectOfType<TransitionManager>().LoadLevel(levelToLoad);
     }
 }
--- VR Game Jam/Assets/Scripts/Interactables/LevelLoadCurrent.cs
+++ VR Game Jam/Assets/Scripts/Interactables/LevelLoadCurrent.cs
@@ -5,8 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoadCurrent : Interactable
 {
+    [SerializeField] private float m_loadDelay = 0.5f;
+    private Animator m_animator;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
     public override void OnInteract()
     {
+        StartCoroutine(Spin());
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator Spin()
+    {
+        m_animator.SetTrigger("spin");
+
+        yield return new WaitForSeconds(m_loadDelay);
+
         FindObjectOfType<TransitionManager>().LoadLevel(SceneManager.GetActiveScene().buildIndex);
     }
 }
